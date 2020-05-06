494998332414cd753f6aac3b52bf88880b356b5d
Varvara Moiseenko <95varya@mail.ru>
2020-05-06 11:53:06 +0500

    feat: change background color on merge

--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -1,40 +1,60 @@
 <template>
-    <div class="game-tile">
-        {{ getValue }}
-    </div>
+  <div class="game-tile" :style="{ background: getColor }">
+    {{ getValue }}
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "gameTile",
-        props: {
-            value: Number
-        },
-        computed: {
-            getValue() {
-                if (this.value == 2048) {
-                    this.$emit('win')
-                }
+export default {
+  name: "gameTile",
+  props: {
+    value: Number,
+  },
+  data: () => ({
+    colors: [
+      "#00C9C6",
+      "#00B4DD",
+      "#0099E4",
+      "#7078D1",
+      "#A14FA1",
+      "#AE2660",
+      "#9F0049",
+      "#BD0060",
+      "#003A3B",
+      "#B275DD",
+    ],
+  }),
+  computed: {
+    getValue() {
+      if (this.value == 2048) {
+        this.$emit("win");
+      }
 
-                return this.value > 0 ? this.value : null
-            }
-        }
-    }
+      return this.value > 0 ? this.value : null;
+    },
+    getColor() {
+      let color;
+      if (this.value > 0 && (this.value & (this.value - 1)) == 0) {
+        let binar = this.value.toString(2);
+        color = this.colors[(binar.length - 2) % this.colors.length];
+      }
+      return color || "#99B2FF";
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .game-tile {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 106.25px;
-    height: 106.25px;
-    margin-right: 15px;
-    background: #95B1AF;
-    // background: #4D7DDB;
-    border-radius: 5px;
-    color: #fff;
-    font-size: 40px;
-    font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 106.25px;
+  height: 106.25px;
+  margin-right: 15px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 40px;
+  font-weight: bold;
 }
 </style>
